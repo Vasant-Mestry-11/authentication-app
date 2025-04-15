cda307da394cbfcef1984d918b2eeaea6568241d
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-15 21:51:03 +0530

    :zap: :art: login auth action

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,8 +1,8 @@
 "use server";
 
 import { createAuthSession } from "@/lib/auth";
-import { hashUserPassword } from "@/lib/hash";
-import { createUser } from "@/lib/user";
+import { hashUserPassword, verifyPassword } from "@/lib/hash";
+import { createUser, getUserByEmailId } from "@/lib/user";
 import { redirect } from "next/navigation";
 
 export async function signUp(prevState, formData) {
@@ -41,4 +41,33 @@ export async function signUp(prevState, formData) {
     }
     throw error
   }
+}
+
+
+export async function login(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const existingUser = getUserByEmailId(email)
+
+  if (!existingUser) {
+    return {
+      errors: {
+        email: `Could not authenticate user, please check your credentials`
+      }
+    }
+  }
+
+  const isValidPassword = verifyPassword(existingUser.password, password);
+
+  if (!isValidPassword) {
+    return {
+      errors: {
+        password: `Could not authenticate user, please check your credentials`
+      }
+    }
+  }
+
+  await createAuthSession(existingUser.id);
+  redirect("/training");
 }
