2d78a9b65a37c55ee7e4085eb44103f02f14d046
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-15 22:06:44 +0530

    :zap: helper auth server action to login and signup combined

--- components/auth-form.js
+++ components/auth-form.js
@@ -1,10 +1,10 @@
 "use client";
-import { signUp } from "@/actions/auth-actions";
+import { auth } from "@/actions/auth-actions";
 import Link from "next/link";
 import { useActionState } from "react";
 
 export default function AuthForm({ mode }) {
-  const [formState, formAction] = useActionState(signUp, {});
+  const [formState, formAction] = useActionState(auth.bind(null, mode), {});
   return (
     <form id="auth-form" action={formAction}>
       <div>
